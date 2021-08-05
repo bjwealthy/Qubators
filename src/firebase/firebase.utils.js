b1172edd337638295c38b93b35d94e48518ad9dd
Mobolaji <bjwealthy@gmail.com>
2021-08-05 08:19:45 +0100

    implemented firebase, used firestore to store users, created user registration with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,13 +12,39 @@ const config = {
     measurementId: "G-FLQ5P9GT47"
   };
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+  
 
-  export default firebase;
+  try{
+    await userRef.set({
+      displayName,
+      email,
+      createdAt,
+      ...additionalData,
+    })
+  }catch(error){
+    alert('error creating user', error.message);
+  }
+}
+return userRef;
+}
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -8,56 +8,56 @@ const SHOP_DATA = [
         {
           id: 1,
           name: 'Yoruba',
-          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rod-long-DRgrzQQsJDA-unsplash_PLOvKQ9Wz9M.jpg?updatedAt=1627303281264',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodyyoruba_he-H43YOCja.jpg?updatedAt=1628015242512',
           price: 'Jan 2005 till Date'
         },
         {
           id: 2,
           name: 'Ibo',
           imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsody_R-zYh-j-yQx.jpg?updatedAt=1627890107978',
-          range: 'Jan 2005 till Date'
+          price: 'Jan 2005 till Date'
         },
         {
           id: 3,
           name: 'Hausa',
-          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/james-coleman-qXTGr9m3oUc-unsplash_GwJ2VvpDoy.jpg?updatedAt=1627303274632',
-          range: 'Jan 2005 till Date'
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodyibo_avwkkTUZYfj.jpg?updatedAt=1628015241901',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 4,
           name: 'Egun',
-          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/alexandra-fuller-hevtI5C3EmU-unsplash_D5f6gvl9oS.jpg?updatedAt=1627303274503',
-          range: 'Jan 2005 till Date'
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodyhausa_orhIjQZKYq.webp?updatedAt=1628015242015',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 5,
           name: 'Jukun',
-          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/vaishakh-pillai-CvWbabexORY-unsplash_8XMQPAWIh-.jpg?updatedAt=1627303272227',
-          range: 'Jan 2005 till Date'
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodyjukun_l4FmLbim2qTB.jpg?updatedAt=1628015742200',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 6,
           name: 'Wada',
-          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/ben-white-qYanN54gIrI-unsplash_VgJmOpKVi.jpg?updatedAt=1627303272129',
-          range: 'Jan 2005 till Date'
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodywada_3O5z_W4A8.jpeg?updatedAt=1628015741340',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 7,
           name: 'English',
           imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/aaron-burden-vKBdY7e7KFk-unsplash_ysiB4Ru3z2.jpg?updatedAt=1627303270816',
-          range: 'Jan 2005 till Date'
+          price: 'Jan 2005 till Date'
         },
         {
           id: 8,
           name: 'Efik',
-          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/alex-woods-pZVi92S-ZMs-unsplash_TG9nuqrL_.jpg?updatedAt=1627303241405',
-          range: 'Jan 2005 till Date'
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodyefik__fhE8DWDKt.jpg?updatedAt=1628015741660',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 9,
           name: 'Ibibio',
-          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-          range: 'Jan 2005 till Date'
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/rhapsodyibibio_lR2VxMhEDBy.jpeg?updatedAt=1628015741282',
+          price: 'Jan 2005 till Date'
         }
       ]
     },
@@ -65,55 +65,55 @@ const SHOP_DATA = [
     {
       id: 2,
       title: 'teevo',
-      routeName: 'books',
+      routeName: 'teevo',
       items: [
         {
           id: 10,
-          name: 'Adidas NMD',
+          name: 'English',
           imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-          range: 'Jan 2005 till Date'
+          price: 'Jan 2005 till Date'
         },
         {
           id: 11,
-          name: 'Adidas Yeezy',
-          imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-          range: 'Jan 2005 till Date'
+          name: 'Afrikaans',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/teevoafrikaans_3x2RDv3Ps.jpg?updatedAt=1628017430479',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 12,
-          name: 'Black Converse',
-          imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-          range: 'Jan 2005 till Date'
+          name: 'Arabic',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/teevoarabic_DbfT3KtBrts.jpg?updatedAt=1628017429177',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 13,
-          name: 'Nike White AirForce',
-          imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-          range: 'Jan 2005 till Date'
+          name: 'Croatian',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/teevocroatian_X5ZdSTjJQs.jpeg?updatedAt=1628017429984',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 14,
-          name: 'Nike Red High Tops',
-          imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-          range: 'Jan 2005 till Date'
+          name: 'Czech',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/teevoczech_Eet4V_MP9ZNi.jpeg?updatedAt=1628017430060',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 15,
-          name: 'Nike Brown High Tops',
-          imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-          range: 'Jan 2005 till Date'
+          name: 'Filipino',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/teevofilipino_g-cP1Z05c.jpg?updatedAt=1628017426956',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 16,
-          name: 'Air Jordan Limited',
-          imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-          range: 'Jan 2005 till Date'
+          name: 'French',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/teevofrench_IgpZOLnAVZw.jpeg?updatedAt=1628017429919',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 17,
-          name: 'Timberlands',
-          imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-          range: 'Jan 2005 till Date'
+          name: 'Italian',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/evoitalian_d0oFgEgc15b.jpeg?updatedAt=1628017430354',
+          price: 'Jan 2005 till Date'
         }
       ]
     },
@@ -122,37 +122,37 @@ const SHOP_DATA = [
     {
       id: 3,
       title: 'early readers',
-      routeName: 'sermons',
+      routeName: 'earlyreaders',
       items: [
         {
           id: 18,
-          name: 'Black Jean Shearling',
-          imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-          range: 'Jan 2005 till Date'
+          name: 'English',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/earlyreadersfilipino_f0s2eLXh0j_.jpg?updatedAt=1628018604375',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 19,
-          name: 'Blue Jean Jacket',
-          imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-          range: 'Jan 2005 till Date'
+          name: 'Afrikaan',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/earlyreadersafrikaans_CN8SxqLeKh.jpg?updatedAt=1628018604634',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 20,
-          name: 'Grey Jean Jacket',
-          imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-          range: 'Jan 2005 till Date'
+          name: 'Arabic',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/earlyreadersarabic_kitAoLqi3TIz.jpg?updatedAt=1628018604493',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 21,
-          name: 'Brown Shearling',
-          imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-          range: 'Jan 2005 till Date'
+          name: 'Croatian',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/earlyreaderscroatian_kBltPzUG0.jpg?updatedAt=1628018604360',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 22,
-          name: 'Tan Trench',
-          imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-          range: 'Jan 2005 till Date'
+          name: 'Czech',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/earlyreadersczech_gFOFpESJU8hY.webp?updatedAt=1628018604860',
+          price: 'Jan 2005 till Date'
         }
       ]
     },
@@ -161,93 +161,93 @@ const SHOP_DATA = [
     {
       id: 4,
       title: 'read to me',
-      routeName: 'songs',
+      routeName: 'readtome',
       items: [
         {
           id: 23,
-          name: 'Blue Tanktop',
-          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-          range: 'Jan 2005 till Date'
+          name: 'English',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readenglish_PANdE75zXOfq.jpeg?updatedAt=1628019300487',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 24,
-          name: 'Floral Blouse',
-          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-          range: 'Jan 2005 till Date'
+          name: 'Afrikaan',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readafrikaan_eI_cVaMMVdj.jpg?updatedAt=1628019300187',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 25,
-          name: 'Floral Dress',
-          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-          range: 'Jan 2005 till Date'
+          name: 'Arabic',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readarabic_lL4p2VJCn.jpg?updatedAt=1628019299805',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 26,
-          name: 'Red Dots Dress',
-          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-          range: 'Jan 2005 till Date'
+          name: 'Croatian',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readcroatian_ZLows8SzRJk.jpg?updatedAt=1628019300461',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 27,
-          name: 'Striped Sweater',
-          imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-          range: 'Jan 2005 till Date'
+          name: 'Czech',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readczech_R6yBaa1OS.jpg?updatedAt=1628019300466',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 28,
-          name: 'Yellow Track Suit',
-          imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-          range: 'Jan 2005 till Date'
+          name: 'Filipino',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readfilipino_xIey9_j0uk.jpeg?updatedAt=1628019300648',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 29,
-          name: 'White Blouse',
-          imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-          range: 'Jan 2005 till Date'
+          name: 'French',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/readfrench_6qnMxbfnqi_.jpeg?updatedAt=1628019304520',
+          price: 'Jan 2005 till Date'
         }
       ]
     },
 //messages
     {
       id: 5,
-      title: 'messages',
-      routeName: 'bibles',
+      title: 'translated messages',
+      routeName: 'translatedmessages',
       items: [
         {
           id: 30,
-          name: 'Camo Down Vest',
-          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-          range: 'Jan 2005 till Date'
+          name: 'English',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/messagesenglish_1Xbp1kbaJVW.jpg?updatedAt=1628019859257',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 31,
-          name: 'Floral T-shirt',
-          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-          range: 'Jan 2005 till Date'
+          name: 'Afrikaan',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/essagesafrikaan_IEQ_m4PTkeMb.png?updatedAt=1628019859890',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 32,
-          name: 'Black & White Longsleeve',
-          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-          range: 'Jan 2005 till Date'
+          name: 'Arabic',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/messagesarabic_1GBXZRPtLG.jpg?updatedAt=1628019859276',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 33,
-          name: 'Pink T-shirt',
-          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-          range: 'Jan 2005 till Date'
+          name: 'Croatian',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/messagescroatian__3t4bhye5.jpg?updatedAt=1628019859291',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 34,
-          name: 'Jean Long Sleeve',
-          imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-          range: 'Jan 2005 till Date'
+          name: 'Czech',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/messagesczech_EHSbRP8oW.jpg?updatedAt=1628019859093',
+          price: 'Jan 2005 till Date'
         },
         {
           id: 35,
-          name: 'Burgundy T-shirt',
-          imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-          range: 'Jan 2005 till Date'
+          name: 'Filipino',
+          imageUrl: 'https://ik.imagekit.io/cyctpb3gf8c/wallpaper/messagesfilipino_MPKShUKGK.jpeg?updatedAt=1628020019515',
+          price: 'Jan 2005 till Date'
         }
       ]
     }
